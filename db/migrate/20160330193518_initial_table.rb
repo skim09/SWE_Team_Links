2032542de357faa1914a3fc181b8fa660c7ac723
fmfliu <yliu04@wesleyan.edu>
2016-03-30 19:37:08 +0000

    add table and controller

---
+++ db/migrate/20160330193518_initial_table.rb
@@ -0,0 +1,11 @@
+class InitialTable < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+    	t.string :name
+    	t.string :url
+    	t.integer :upvotes
+    	t.string :category
+      t.timestamps
+    end
+  end
+end
