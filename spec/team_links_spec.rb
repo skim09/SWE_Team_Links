9da8c034d697904803034f07b55cb59fc9351857
Joseph Koshakow <jkoshakow@wesleyan.edu>
2016-04-03 17:42:22 +0000

    added skeleton for expanding and collapsing categories in rspec

--- spec/team_links_spec.rb
+++ spec/team_links_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+describe LinksController, :type => :controller do
+   
+   describe 'add new link' do
+    it 'should add a new link' do
+      @fake_link = double("Link").as_null_object
+      allow(Links).to receive(:create!).and_return(@fake_link)
+      post :create, :link => {}
+      expect(response).to redirect_to(links_path)
+    end
+  end
+  
+  
+    
+end
