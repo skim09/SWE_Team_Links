88007fc02f86228935fdec38fefa43231808e6b9
Sangwon Kim <skim09@wesleyan.edu>
2016-04-03 19:23:09 +0000

    CSS for adding links page and basic back-end

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,39 @@
 class LinksController < ApplicationController
+    
+    def link_params
+        params.require(:link).permit(:name, :url, :upvotes, :category)
+    end
+    
+    def show
+        id = params[:id] # retrieve link ID from URI route
+        @link = Link.find(id) # look up link by unique ID
+        # will render app/views/links/show.<extenstion> by default
+    end
+    
+    def index
+        @links = Link
+    end
+    
+    def new
+    # default: render 'new' template
+    end
+
+    def create
+        @link = Link.create!(link_params)
+        flash[:notice] = "#{@link.name} was successfully submitted."
+        redirect_to links_path
+    end
+    
+    def edit
+        @link = Link.find params[:id]
+    end
+    
+    def update
+        redirect_to links_path
+    end
+
+    def destroy
+        redirect_to links_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :links
+  root :to => redirect('/links')
 end
