2bcb5e6530d7ddaed6f0defda7bad6ae04308aa4
Joseph Koshakow <jkoshakow@wesleyan.edu>
2016-04-03 20:23:12 +0000

    change Links to Link

--- spec/team_links_spec.rb
+++ spec/team_links_spec.rb
@@ -7,7 +7,7 @@ describe LinksController, :type => :controller do
     context 'all fields correct' do
       it 'should add a new link' do
         @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search")
-        allow(Links).to receive(:create!).and_return(@fake_link)
+        allow(Link).to receive(:create!).and_return(@fake_link)
         #this line should be edited probably
         post :create, :link => @fake_link
         expect(response).to redirect_to(links_path)
@@ -18,7 +18,7 @@ describe LinksController, :type => :controller do
       it 'should reject the url' do
         @fake_link = double("Link", :name => "Google", :url => "google", :category => "search")
         #this line should be edited
-        allow(Links).to receive(:create!).and_return(false)
+        allow(Link).to receive(:create!).and_return(false)
         post :create, :link => @fake_link
         expect(response).to redirect_to(new_link_path)
       end
@@ -28,20 +28,22 @@ describe LinksController, :type => :controller do
       it 'should reject the URL' do
         @fake_link = double("Link", :name => "", :url => "www.google.com", :category => "search")
         #this line should be edited
-        allow(Links).to receive(:create!).and_return(false)
+        allow(Link).to receive(:create!).and_return(false)
         post :create, :link => @fake_link
         expect(response).to redirect_to(new_link_path)
       end
     end
     
     context 'not unique URL' do
-      #add google to the fake database
-      @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search")
-      allow(Links).to receive(:create!).and_return(false)
-      #this line should be edited probably
-      post :create, :link => @fake_link
-      #maybe should go back to new_link_path
-      expect(response).to redirect_to(new_link_path)
+        it 'should reject the URL' do
+        #add google to the fake database
+        @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search")
+        allow(Link).to receive(:create!).and_return(false)
+        #this line should be edited probably
+        post :create, :link => @fake_link
+        #maybe should go back to new_link_path
+        expect(response).to redirect_to(new_link_path)
+      end
     end
   end
   
