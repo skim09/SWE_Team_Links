698f9f903eca34c440a5fcc5554d64aca8bbdf72
Joseph Koshakow <jkoshakow@wesleyan.edu>
2016-04-04 19:57:35 +0000

    Added a validation to make sure the link is properly formatted

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,20 @@
 require 'uri'
 
 class Link < ActiveRecord::Base
-  validates :url, presence: {:message => "You need to enter a URL"}
-  validates :name, presence: {:message => "You need to give the URL a name"}
+  validates :url, presence: true, :message => "You need to enter a URL"
+  validates :name, presence: true, :message => "You need to give the URL a name"
+  validates :url, format: { with: URI.regexp, :message => "Not a properly formatted URL, make sure to include \"http://\" or \"https://\""}
+  validates :url, uniqueness: true, :message => "This Link already exists" 
     
   def self.all_categories
     %w(Internships Grants Jobs Inspiration Other)
   end
+  
+  # def valid_url
+  #   uri = URI.parse(url)
+  #   uri.kind_of?(URI::HTTP)
+  # rescue URI::InvalidURIError
+  #   false
+  # end
+  
 end
