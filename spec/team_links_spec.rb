c8709c3d0ca6959763cd0f8d0e1d590e701a2900
Joseph Koshakow <jkoshakow@wesleyan.edu>
2016-04-04 20:38:02 +0000

    cleaned up rspec file

--- spec/team_links_spec.rb
+++ spec/team_links_spec.rb
@@ -21,22 +21,12 @@ describe LinksController, :type => :controller do
     context 'invalid URL' do
       it 'should reject the url' do
         Link.new(:name => "Google", :url => "google", :category => "search", :upvotes => 0).should_not be_valid
-        # @fake_link = double("Link", :name => "Google", :url => "google", :category => "search", :upvotes => 0).as_null_object
-        # #this line should be edited
-        # expect(Link).to receive(:create!).and_return(false)
-        # post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
-        # expect(response).to redirect_to(links_path)
       end
     end
     
     context 'empty name' do
       it 'should reject the URL' do
         Link.new(:name => "", :url => "http://www.google.com", :category => "search", :upvotes => 0).should_not be_valid
-        # @fake_link = double("Link", :name => "", :url => "http://www.google.com", :category => "search", :upvotes => 0)
-        # #this line should be edited
-        # allow(Link).to receive(:create!).and_return(@fake_link)
-        # post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
-        # expect(response).to redirect_to(links_path)
       end
     end
     
@@ -50,26 +40,7 @@ describe LinksController, :type => :controller do
         it 'should reject the URL' do
           Link.create!(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)
           Link.new(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0).should_not be_valid
-        # #add google to the fake database
-        # @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search", :upvotes => 0)
-        # allow(Link).to receive(:create!).and_return(@fake_link)
-        # #this line should be edited probably
-        # post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
-        # #maybe should go back to new_link_path
-        # expect(response).to redirect_to(links_path)
       end
     end
-  
-  # describe 'expanding a category' do
-  #   it 'should show a category after clicking it' do
-      
-  #   end
-  # end
-  
-  # describe 'collapsing a category' do
-  #   it 'should hide a category after clicking it' do
-        
-  #   end
   end
-    
 end
