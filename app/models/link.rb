4df551d8d4cc406bc94d97426cda9e84a9d27a64
Joseph Koshakow <jkoshakow@wesleyan.edu>
2016-04-04 19:23:23 +0000

    only rspec test is adding a link now. added validates to the Model for name and url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,14 +1,8 @@
 require 'uri'
 
 class Link < ActiveRecord::Base
-  def create!(link_params)
-    #add invalid url
-    if(link_params.name == "" or link_params.name == nil or not Link.where(:url => link_params.url).blank?)
-      return false    
-    else
-      super
-    end
-  end
+  validates :url, presence: {:message => "You need to enter a URL"}
+  validates :name, presence: {:message => "You need to give the URL a name"}
     
   def self.all_categories
     %w(Internships Grants Jobs Inspiration Other)
--- spec/team_links_spec.rb
+++ spec/team_links_spec.rb
@@ -2,61 +2,60 @@ require 'rails_helper'
 
 describe LinksController, :type => :controller do
    
-   describe 'add new link' do
+  describe 'add new link' do
     
     context 'all fields correct' do
       it 'should add a new link' do
         @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search", :upvotes => 0)
         allow(Link).to receive(:create!).and_return(@fake_link)
-        #this line should be edited probably
         post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
         expect(response).to redirect_to(links_path)
       end
     end
     
-    context 'invalid URL' do
-      it 'should reject the url' do
-        @fake_link = double("Link", :name => "Google", :url => "google", :category => "search", :upvotes => 0)
-        #this line should be edited
-        allow(Link).to receive(:create!).and_return(false)
-        post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
-        expect(response).to redirect_to(new_link_path)
-      end
-    end
+  #   context 'invalid URL' do
+  #     it 'should reject the url' do
+  #       @fake_link = double("Link", :name => "Google", :url => "google", :category => "search", :upvotes => 0).as_null_object
+  #       #this line should be edited
+  #       expect(Link).to receive(:create!).and_return(false)
+  #       post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
+  #       expect(response).to redirect_to(new_link_path)
+  #     end
+  #   end
     
-    context 'empty name' do
-      it 'should reject the URL' do
-        @fake_link = double("Link", :name => "", :url => "www.google.com", :category => "search", :upvotes => 0)
-        #this line should be edited
-        allow(Link).to receive(:create!).and_return(false)
-        post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
-        expect(response).to redirect_to(new_link_path)
-      end
-    end
+  #   context 'empty name' do
+  #     it 'should reject the URL' do
+  #       @fake_link = double("Link", :name => "", :url => "www.google.com", :category => "search", :upvotes => 0)
+  #       #this line should be edited
+  #       allow(Link).to receive(:create!).and_return(false)
+  #       post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
+  #       expect(response).to redirect_to(new_link_path)
+  #     end
+  #   end
     
-    context 'not unique URL' do
-        it 'should reject the URL' do
-        #add google to the fake database
-        @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search", :upvotes => 0)
-        allow(Link).to receive(:create!).and_return(false)
-        #this line should be edited probably
-        post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
-        #maybe should go back to new_link_path
-        expect(response).to redirect_to(new_link_path)
-      end
-    end
-  end
+  #   context 'not unique URL' do
+  #       it 'should reject the URL' do
+  #       #add google to the fake database
+  #       @fake_link = double("Link", :name => "Google", :url => "www.google.com", :category => "search", :upvotes => 0)
+  #       allow(Link).to receive(:create!).and_return(false)
+  #       #this line should be edited probably
+  #       post :create, :link => {:name => @fake_link.name, :url => @fake_link.url, :category => @fake_link.category, :upvotes => @fake_link.upvotes} 
+  #       #maybe should go back to new_link_path
+  #       expect(response).to redirect_to(new_link_path)
+  #     end
+  #   end
+  # end
   
-  describe 'expanding a category' do
-    it 'should show a category after clicking it' do
+  # describe 'expanding a category' do
+  #   it 'should show a category after clicking it' do
       
-    end
-  end
+  #   end
+  # end
   
-  describe 'collapsing a category' do
-    it 'should hide a category after clicking it' do
+  # describe 'collapsing a category' do
+  #   it 'should hide a category after clicking it' do
         
-    end
+  #   end
   end
     
 end
