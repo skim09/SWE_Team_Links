0c0ee8f5f88c14e025cae669ed601bbf908a06e4
Joseph Koshakow <jkoshakow@wesleyan.edu>
2016-04-06 17:41:46 +0000

    rspec at 89%

--- spec/team_links_spec.rb
+++ spec/team_links_spec.rb
@@ -21,18 +21,24 @@ describe LinksController, :type => :controller do
     context 'invalid URL' do
       it 'should reject the url' do
         expect(Link.new(:name => "Google", :url => "google", :category => "search", :upvotes => 0)).to_not be_valid
+        post :create, :link => {:name => "Google", :url => "google.com", :category => "search", :upvotes => 0} 
+        expect(response).to redirect_to(new_link_path)
       end
     end
     
     context 'empty name' do
       it 'should reject the URL' do
         expect(Link.new(:name => "", :url => "http://www.google.com", :category => "search", :upvotes => 0)).to_not be_valid
+        post :create, :link => {:name => "", :url => "http://www.google.com", :category => "search", :upvotes => 0} 
+        expect(response).to redirect_to(new_link_path)
       end
     end
     
     context 'empty url' do
       it 'should reject the URL' do
         expect(Link.new(:name => "google", :url => "", :category => "search", :upvotes=> 0)).to_not be_valid
+        post :create, :link => {:name => "Google", :url => "", :category => "search", :upvotes => 0} 
+        expect(response).to redirect_to(new_link_path)
       end
     end
     
@@ -40,7 +46,50 @@ describe LinksController, :type => :controller do
         it 'should reject the URL' do
           Link.create!(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)
           expect(Link.new(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)).to_not be_valid
+          post :create, :link => {:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0} 
+          expect(response).to redirect_to(new_link_path)
       end
     end
   end
+  
+  describe 'index' do
+    it 'should display all links' do
+      get :index
+    end
+  end
+  
+  describe 'show' do
+    it 'should show a link' do
+      Link.create!(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)
+      get :show, :id => 1
+    end
+  end
+  
+  describe 'new' do
+    it 'should show the new page' do
+      get :new
+    end
+  end
+  
+  describe 'edit' do
+    it 'should show the edit page' do
+      Link.create!(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)
+      get :edit, :id => 1
+    end
+  end
+  
+  describe 'update' do
+    it 'should go to the update page' do
+      Link.create!(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)
+      get :update, :id => 1
+    end
+  end
+  
+  describe 'destroy' do
+    it 'should destroy a link' do
+      Link.create!(:name => "Google", :url => "http://www.google.com", :category => "search", :upvotes => 0)
+      get :destroy, :id => 1
+    end
+  end
+  
 end
