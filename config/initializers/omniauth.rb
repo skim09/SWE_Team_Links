4ea9e0a7e0ced60a351e618225a7c73e99066d7a
Sangwon Kim <skim09@wesleyan.edu>
2016-04-25 01:14:54 +0000

    changed omniauth for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1041961356511-l1buchtlh22qsv9o5k6cg34qt8tbk1o1.apps.googleusercontent.com', '2ewZjRMY4tbqbg8tk9abGbTF', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '1041961356511-t8gej6d93m0gl77usen4stfe7vi8vpo1.apps.googleusercontent.com', 'zYP6Oi9m4rimY_5FFPsiGYSY', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
